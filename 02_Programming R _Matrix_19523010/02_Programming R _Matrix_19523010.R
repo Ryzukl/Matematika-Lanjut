6f842e6c0aef36152e431879a8c1147fa0734d59
Ryzukl <71205853+Ryzukl@users.noreply.github.com>
2020-10-03 09:40:32 +0700

    Update 02_Programming R _Matrix_19523010.R

--- 02_Programming R _Matrix_19523010/02_Programming R _Matrix_19523010.R
+++ 02_Programming R _Matrix_19523010/02_Programming R _Matrix_19523010.R
@@ -1,12 +1,12 @@
 #Exercise
-A <??? matrix ( data = c ( 1 , 2 , 3 , 4 ) , 2, 2, byrow = TRUE )
+A <- matrix ( data = c ( 1 , 2 , 3 , 4 ) , 2, 2, byrow = TRUE )
 
 #Exercise
 A[-1,]
 A[,-2]
 
 #Exercise
-N <??? mat rix ( c ( 1 : 9 ) , 3 , 3 , TRUE)
+N <- mat rix ( c ( 1 : 9 ) , 3 , 3 , TRUE)
 N[N > 3] <- 2
 N
 
@@ -15,7 +15,7 @@ N
 
 values <- sample (1000, 100, replace = FALSE, prob = NULL)
 
-G <??? matrix( values, 10, 10, byrow = TRUE )
+G <- matrix( values, 10, 10, byrow = TRUE )
 
 H <- t(G)
 J <- G + H
