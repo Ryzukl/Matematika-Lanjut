c1771d48431cd958738cbb2cb73a9be4bed2e39f
Ryzukl <71205853+Ryzukl@users.noreply.github.com>
2020-10-03 09:39:55 +0700

    Add files via upload

---
+++ 02_Programming R _Matrix_19523010/02_Programming R _Matrix_19523010.R
@@ -0,0 +1,27 @@
+#Exercise
+A <??? matrix ( data = c ( 1 , 2 , 3 , 4 ) , 2, 2, byrow = TRUE )
+
+#Exercise
+A[-1,]
+A[,-2]
+
+#Exercise
+N <??? mat rix ( c ( 1 : 9 ) , 3 , 3 , TRUE)
+N[N > 3] <- 2
+N
+
+#Excercise
+?sample
+
+values <- sample (1000, 100, replace = FALSE, prob = NULL)
+
+G <??? matrix( values, 10, 10, byrow = TRUE )
+
+H <- t(G)
+J <- G + H
+det(G)
+det(H)
+det(J)
+K <- cbind(G[,1:5], J[,1:5])
+G %*% solve(G)
+
